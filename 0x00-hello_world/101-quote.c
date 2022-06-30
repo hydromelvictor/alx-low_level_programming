5cc57fa986c70035315cb9b2d1dd0fca759b08ae
harkinder-dark <dolk_svictor@gmail.com>
2022-06-30 15:53:52 +0000

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*	main -  print and that piece of art is useful" - Dora Korpar, 2015-10-19"
+*	main - print and that piece of art is useful" - Dora Korpar, 2015-10-19"
+*	standart error
 *	Return: 1
 */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
