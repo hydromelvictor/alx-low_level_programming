d212bfc2e2b0486c0776a9204f7cef7ca1e3d508
harkinder-dark <dolk_svictor@gmail.com>
2022-06-30 14:28:32 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+/**
+*	main - print of  Programming is like building a multilingual puzzle
+*	Return: value 0
+*/
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0) ;
 }
