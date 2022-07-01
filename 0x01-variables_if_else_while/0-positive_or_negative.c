7fdaf94d42361a4fd1429b788ea28641a429c73d
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 11:56:28 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /* 
-* main - This program will assign a random number to the variable n  each time it is executed
+* main - This program will assign a random number to the variable n  
+*each time it is executed
 * Return : always 0
 */
 int main(void)
@@ -13,16 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+		printf("%d is zero\n", n);
 	else
-	{
-	printf("%d is negative\n", n);
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
