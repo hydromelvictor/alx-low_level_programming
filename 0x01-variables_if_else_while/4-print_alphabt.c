ebe21851e32873ef8f04e9594981c36582de6deb
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 17:02:35 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
-*main -> 
-*
+*main -> Write a program that prints the alphabet in lowercase
 *Return; always 0
 */
 int main(void)
 {
-	char str;
+char str;
 	for (str = 'a'; str <= 'z'; str++)
 	{
-		if (str !='q' && str != 'e')
+		if (str != 'q' && str != 'e')
 		{
 		putchar(str);
 		}
