64b6b75d80e5e991948212f0c14284bc9eb891cd
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 17:24:39 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /***
-*main -> Write a program that prints all single digit numbers of base 
+*main -> Write a program that prints all single digit numbers of base
 *10 starting from 0
 *Return: always 0
 */
 int main(void)
 {
 int str;
-	for( str= 48; str<=57; str++)
+	for ( str = 48; str<= 57; str++)
 	{
 		putchar(str);
 	}
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
-*main -> Write a program that prints the lowercase alphabet in reverse 
+*main -> Write a program that prints the lowercase alphabet in reverse
 *Return: always 0
 */
 int main(void)
 {
 char str;
-	for (str = 'z'; str <= 'a'; str++)
+	for (str = 'z'; str => 'a'; str++)
 	{
 	putchar(str);
 	}
