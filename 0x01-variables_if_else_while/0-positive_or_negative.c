1b776a0c9855f9e1e332cf8e090c412adcbb5207
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 10:57:14 +0000

    conditionnal programming

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* *
+*	main - function assign a random number 
+*	Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d : ", $n);
+	if (n > 0)
+		printf(" est positif\n");
+	else if ( n == 0){
+		printf(" est zero\n");
+	else
+		printf(" est negatif\n");
+	
+	return (0);
+}
