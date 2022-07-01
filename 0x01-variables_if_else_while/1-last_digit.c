1798207bf4c799b0f987d000e3c7b625755c3d91
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 15:41:15 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,17 +9,17 @@
  */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d  is  %d ", n, (n%10) );
-if ((n%10) > 5)
-	printf(" and greater than 5");
-if ((n%10)  == 0)
-	printf("0");
-if (n(n%10) < 6 && (n%10)  != 0)
-	printf("less than 6 not 0");
-printf("\n");
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d  is  %d ", n, (n%10) );
+	if ((n%10) > 5)
+		printf(" and greater than 5");
+	if ((n%10)  == 0)
+		printf("0");
+	if ((n%10) < 6 && (n%10)  != 0)
+		printf("less than 6 not 0");
+	printf("\n");
+	return (0);
 }
