6c721dc93027828904296c8fe8647727777624b1
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 17:08:05 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-/**********/
+
+/**
+*main -> Write a program that prints all single digit numbers of base 10 
+*starting from 0
+*Return: always 0
+*/
 int main(void)
 {
-	int i;
-	for( i = 0; i<10; i++)
+int i;
+	for ( i = 0; i < 10; i++)
 	{
 		printf("%d\n", i);
 	}
-	printf("\n");
 	return (0);
 }
