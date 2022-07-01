06863319341f85dfaa5276687998db3217f6bd92
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 15:45:56 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- *main - > This program will assign a random number to the variable n each time 
- *it is executed
+ *main - > This program will assign a random number to the variable n
+ *each time it is executed
  *Return: always 0
  */
 int main(void)
@@ -13,13 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d  is  %d ", n, (n%10) );
-	if ((n%10) > 5)
+	printf("Last digit of %d  is  %d " , n, (n % 10) );
+	if ((n % 10) > 5)
 		printf(" and greater than 5");
-	if ((n%10)  == 0)
-		printf("0");
-	if ((n%10) < 6 && (n%10)  != 0)
-		printf("less than 6 not 0");
+	if ((n % 10)  == 0)
+		printf("0 and is 0");
+	if ((n % 10) < 6 && (n % 10)  != 0)
+		printf("less than 6 and not 0");
 	printf("\n");
 	return (0);
 }
