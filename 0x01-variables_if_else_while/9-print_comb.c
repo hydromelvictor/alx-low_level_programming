15e1d27e2edecd3210f92372f26d744af7b6f9de
harkinder-dark <dolk_svictor@gmail.com>
2022-07-01 17:45:46 +0000

    conditionnal programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,19 @@
 #include <stdio.h> 
 
 /**
-*main -> Write a program that prints all the numbers of base 16 in lowercase
+*main -> Write a program that prints all possible combinations of
+*single-digit numbers.
 *Return: always 0
 */
 int main(void)
 {
-char str;
 int i;
 	for (i = 48; i <= 57; i++)
 	{
 	putchar(i);
+		if(i != 57)
+			putchar(44);
+			putchar(32);
 	}
 	for(str = 'a'; str <= 'f'; str++)
 	{
