3c4b8f82716da8b9f150e002f6a208c91db95934
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 15:19:27 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-*_islower -> Write a function that checks for lowercase character.
+*_isalpha -> Write a function that checks for lowercase character.
+*@c; is a integer
 *Return: 0 or 1
 */
 int _isalpha(int c)
 {
-	if (c <= 122 && c >= 97) && (c <= 90 && c >= 65))
-        	return (1);
-	return (0);
+if ((c <= 122 && c >= 97) && (c <= 90 && c >= 65))
+	return (1);
+return (0);
 }
