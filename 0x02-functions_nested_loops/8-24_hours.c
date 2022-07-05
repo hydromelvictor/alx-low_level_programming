c1d629332599c356cb947a1eeb985db995dc36dd
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 20:18:56 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
-*jack_bauer -> Write a function that prints every minute of the day of Jack Bauer
+*jack_bauer -> Write a function that prints every minute of the day of 
+*Jack Bauer
 */
 void jack_bauer(void)
 {
 int i, j;
-	for (i = 0; i < 24; i++)
-	{
-    		for (j = 0; j < 60; j++) 
-    		{
-    			if (i < 10)
-    			{
-        			_putchar('0');
-        			_putchar(i + '0');
-        		}
-        		else if (i >= 10)
-        		{
-        			_putchar((i / 10) + '0');
-        			_putchar((i % 10) + '0');
-        		}
-        		if (j < 10)
-        		{
-        			_putchar(':');
-        			_putchar('0');
-        			 _putchar(j);
-        		}
-        		else if ( j > 10)
-        		{
-    				_putchar(':');
-    				_putchar((j / 10) + '0');
-    				_putchar((j % 10) + '0');
-    			}
-       		}
-       		_putchar(10);
-    	}
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+{
+_putchar('0');
+_putchar(i + '0');
+}
+else if (i >= 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(j);
+}
+else if ( j > 10)
+{
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+}
+}
 }
