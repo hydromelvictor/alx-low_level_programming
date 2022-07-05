24ab5b3cd866f9903f1afaab8bc3cf80df08b718
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 17:47:07 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,10 @@
 
 /**
 *print_last_digit -> Write a function that prints the last digit of a number
+*@n: parameter
 *Return: n%10
 */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	return (n % 10);
 }
