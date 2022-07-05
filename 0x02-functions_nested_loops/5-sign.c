5c356c141cd71d4a6a92bc26139cc1952258d672
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 17:30:30 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
 *print_sign -> Write a function that prints the sign of a number.
+@n: parameter
 *Return: -1 or 0 or 1
 */
 int print_sign(int n)
@@ -16,5 +17,5 @@ if (n < 0)
 	_putchar(45);
 	return (-1);
 }
-return (0);
+return (00);
 }
