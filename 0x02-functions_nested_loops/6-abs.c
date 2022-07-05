e571f3a6ab02e88d62e9cda24afeed9796a8675b
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 17:44:11 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,20 +2,18 @@
 
 /**
 *_abs -> Write a function that computes the absolute value of an integer
+@n: parameter
 *Return: n
 */
 int _abs(int n)
 {
-if (n > 0)
-{
-return (n);
-}
-if (n < 0)
-{
-return (-n);
-}
-if(n == 0)
-{
-return (0);
-}
+	if (n > 0)
+	{
+	return (n);
+	}
+	if (n < 0)
+	{
+	return (-n);
+	}
+	return (0);
 }
