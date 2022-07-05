3e1999e14ae2021f5e9aba9f570fa06acbe4c4e1
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 15:10:19 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,9 @@ void print_alphabet_x10(void)
 {
 int i, j;
 for (j = 0; j < 10; j++)
-	for (i = 97; i <= 122; i++)
-		_putchar(i);
-	_putchar(10);
+{
+for (i = 97; i <= 122; i++)
+	_putchar(i);
+_putchar(10);
+}
 }
