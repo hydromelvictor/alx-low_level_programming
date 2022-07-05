f6c9a3e36a60b13aa6ced9874be334d915051efe
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 20:59:54 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,9 @@
 int print_last_digit(int n)
 {
 int m;
-	if (n < 0)
-		n = -n;
-	m = n % 10;
-	_putchar(m + '0');
-	return (m);
+if (n < 0)
+	n = -n;
+m = n % 10;
+_putchar(m + '0');
+return (m);
 }
