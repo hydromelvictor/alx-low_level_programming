bdd8b0ae22d4b7efa5b12d42358b732007cc3e2a
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 19:56:21 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,19 @@
 void jack_bauer(void)
 {
 int i, j;
-	for (i = 0; i < 24; i++){
+	for (i = 0; i < 24; i++)
+	{
     		if (i < 10)
-        		_putchar('0');
+        		_putchar(i + '0');
     		_putchar(i);
-   		 _putchar(':');
+   		_putchar(':');
 
-    		for (j = 0; j < 60; j++) {
+    		for (j = 0; j < 60; j++) 
+    		{
         		if (j < 10)
-            			_putchar('0');
-        	}
-        	_putchar(j);
-    }
+            			_putchar(j + '0');
+       			 _putchar(j);
+       		}
+       		_putchar(10);
+    	}
 }
