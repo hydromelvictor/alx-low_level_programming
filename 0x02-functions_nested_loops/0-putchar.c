998e70f744bef25787dabb8acdc608e03a148f2c
harkinder-dark <dolk_svictor@gmail.com>
2022-07-05 14:50:55 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,10 @@ int main(void)
 {
 char mot[8] = "_putchar";
 	int i;
-    	for (i = 0; i < 8; i++)
-        	_putchar(mot[i]);
+	for (i = 0; i < 8; i++)
+	{
+        _putchar(mot[i]);
+        }
 	_putchar(10);
 return (0);
 }
