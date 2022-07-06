dee9eac7e44a23a3284c3bfd11b973eac53274c9
harkinder-dark <dolk_svictor@gmail.com>
2022-07-06 06:35:00 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,6 +2,20 @@
 #include "main.h"
 
 /**
+*fibonacci -> fibonacci number genereted
+*@n: parameter
+*Return: fibonacci number
+*/
+int fibonacci(int n)
+{
+if (n == 0 || n == 1)
+	return (1);
+if (n == 2)
+	return (2);
+return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+/**
 *main -> Write a program that prints the first 50 Fibonacci numbers
 *Return: always 0
 */
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,6 +2,20 @@
 #include "main.h"
 
 /**
+*fibonacci -> fibonacci number genereted
+*@n: parameter
+*Return: fibonacci number
+*/
+int fibonacci(int n)
+{
+if (n == 0 || n == 1)
+	return (1);
+if (n == 2)
+	return (2);
+return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+/**
 *main -> Write a program that finds and prints the first 98 Fibonacci
 *numbers
 *Return: always 0
