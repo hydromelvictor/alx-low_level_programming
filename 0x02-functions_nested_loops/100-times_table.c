4f6a99b5204da50f004fbdfe02c7b2a644c8e1b1
harkinder-dark <dolk_svictor@gmail.com>
2022-07-06 14:18:47 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,27 +7,34 @@
 void print_times_table(int n)
 {
 int i, j;
-if (n <= 15 && n > 0)
-{
-for (i = 0; i < n; i++)
-{
-for(j = 0; j < n; j++)
-{
-	int k  = i * j;
-	if(k < 10)
-	{
-	_putchar(k + '0');
-	_putchar(',');
-	_putchar(' ');
-	}
-	else
-	{
-	_putchar((k / 10) + '0');
-	_putchar((k % 10) + '0');
-	_putchar(',');
-	_putchar(' ');
-	}
-}
-}
-}
+    if (n <= 15 && n > 0)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                int k  = i * j;
+                if (k < 10)
+                {
+                if(j !=0)
+                	_putchar(32);
+                    _putchar(k + '0');
+                    _putchar(',');
+                    if(j != 9)
+                   	 _putchar(32);
+                }
+                else
+                {
+                if(j !=0)
+                	_putchar(32);
+                    _putchar((k / 10) + '0');
+                    _putchar((k % 10) + '0');
+                    _putchar(',');
+                    if(j != 9)
+                   	 _putchar(32);
+                }
+            }
+            _putchar(10);
+        }
+    }
 }
