7c91c1140373fdaf9564d4f9ec5d231ce639975a
harkinder-dark <dolk_svictor@gmail.com>
2022-07-06 15:03:20 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,50 @@
 void print_times_table(int n)
 {
 int i, j;
-    if (n <= 15 && n > 0)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                int k  = i * j;
-                if (k < 10)
-                {
-                if(j !=0)
-                	_putchar(32);
-                    _putchar(k + '0');
-                    if(j != n)
-                    	_putchar(',');
-                   	_putchar(32);
-                   	_putchar(32);
-                }
-                else if(k > 10)
-                {
-                    _putchar((k / 10) + '0');
-                    _putchar((k % 10) + '0');
-                    if(j != n)
-                    	_putchar(',');
-                   	_putchar(32);
-                   	_putchar(32);
-                }
-                else
-                {
-                    _putchar(((k / 10) / 10) + '0');
-                    _putchar((((k / 10) / 10) % 10) + '0');
-                    _putchar((k % 10) + '0');
-                    if(j != n)
-                    	_putchar(',');
-                   	_putchar(32);
-                   	_putchar(32);
-                }
-            }
-            _putchar(10);
-        }
-    }
+if (n <= 15 && n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int k  = i * j;
+if (k < 10)
+{
+if (j != 0)
+	_putchar(32);
+_putchar(k + '0');
+if (j != n)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+}
+}
+else if(k > 10 && k < 100)
+{
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+if (j != n)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+}
+}
+else
+{
+_putchar(((k / 10) / 10) + '0');
+_putchar((((k / 10) / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+if(j != n)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+}
+}
+}
+_putchar(10);
+}
+}
 }
