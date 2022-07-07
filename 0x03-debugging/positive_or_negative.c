32eb04d49819206841cc59c3d29a4e4d8811fd18
harkinder-dark <dolk_svictor@gmail.com>
2022-07-07 06:45:14 +0000

    debugging folder programming

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+#include "main.h"
 /**
-*main -> This program will assign a random number to the variable n
-*each time it is executed
-*Return: always 0
+*positive_or_negative -> This program will assign a random number to
+*the variable n each time it is executed
+*@i: parameter
 */
 void positive_or_negative(int i)
 {
