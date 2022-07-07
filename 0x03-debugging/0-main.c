7312932e47b29e8bbce16e0ae1eec29e36967ce0
harkinder-dark <dolk_svictor@gmail.com>
2022-07-07 18:45:52 +0000

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,13 +5,12 @@
 *main - tests function that prints if integer is positive or negative
 *Return: 0
 */
-
 int main(void)
 {
 int i;
 
 i = 0;
 positive_or_negative(i);
-printf("\n");
+
 return (0);
 }
