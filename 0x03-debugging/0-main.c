d6804e62e384ba999e949bde7d95bafc47292688
harkinder-dark <dolk_svictor@gmail.com>
2022-07-08 03:14:22 +0000

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-i = 0;
+i = 1024;
 positive_or_negative(i);
 
 return (0);
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,13 +4,11 @@
 *main - causes an infinite loop
 *Return: 0
 */
-
 int main(void)
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 /*
 *while (i < 10)
@@ -19,6 +17,5 @@ i = 0;
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
