7e3fa131b0fd019ee1f64576f4c85420525d027f
harkinder-dark <dolk_svictor@gmail.com>
2022-07-08 04:42:32 +0000

    nested lopps

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,15 @@ void print_square(int size)
 {
 int i, j;
 
-if(size < 0)
+if (size < 0)
 {
 _putchar(10);
 }
 else
 {
-for(i = 1; i <= size; i++)
+for (i = 1; i <= size; i++)
 {
-for(j = 1; j <= size; j++)
+for (j = 1; j <= size; j++)
 {
 _putchar('#');
 }
