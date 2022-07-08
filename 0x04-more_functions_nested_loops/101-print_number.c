e3ef3c9a70209aa4302b96a3f6f907df9118433d
harkinder-dark <dolk_svictor@gmail.com>
2022-07-08 05:19:46 +0000

    nested lopps

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,6 +19,10 @@ void print_number(int n)
 {
 int a;
 int i = 0;
+if(n < 0)
+{
+n = -n;
+}
 if (n >= 10)
 {
 while (n >= 10)
@@ -36,11 +40,10 @@ a = a * 10;
 }
 n = n - a;
 }
-_putchar(n + '0');
 }
 else
 {
-if (n < 0)
+if (n < 10)
 {
 _putchar('-');
 _putchar(n + '0');
