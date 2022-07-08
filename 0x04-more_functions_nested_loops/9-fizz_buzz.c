61a7a56bbb470c52f3da887ab314a175293044f2
harkinder-dark <dolk_svictor@gmail.com>
2022-07-08 04:48:42 +0000

    nested lopps

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,27 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
+if (i < 100)
+{
+printf(" ");
+}
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf("Buzz");
+if (i < 100)
+{
+printf(" ");
+}
 }
 else if (i % 15 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
+if (i < 100)
+{
+printf(" ");
+}
 }
 else
 {
