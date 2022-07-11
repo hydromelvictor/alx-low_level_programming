9cdc0d35d2a35e87ffd2b15f4c6c6f103db9b456
harkinder-dark <dolk_svictor@gmail.com>
2022-07-11 23:55:42 +0000

    arrays strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,15 @@
 void print_array(int *a, int n)
 {
 int i;
-if( n > 0)
+if ( n > 0)
 {
 for (i = 0; i < n; i++)
 {
-if(i < n - 1)
+if (i < n - 1)
 {
 printf("%d, ", a[i]);
 }
-else 
+else
 {
 printf("%d", a[i]);
 }
