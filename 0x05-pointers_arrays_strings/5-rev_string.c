ffb519411450f1e0ef1496c3478a6f18526b33c7
harkinder-dark <dolk_svictor@gmail.com>
2022-07-11 22:37:51 +0000

    arrays strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,15 @@
 */
 void rev_string(char *s)
 {
-int i = 0,j = 0;
-while (s[i] != '\0')
+int i = 0;
+while(s[i] != '\0')
 {
 i++;
 }
-j = i - 1;
-while (j >= 0)
+i = i - 1;
+while(i >= 0)
 {
-_putchar(s[j]);
-j--;
+_putchar(s[i]);
+i--;
 }
 }
