260127d531fdeb62e372b521ddda4570aad517af
harkinder-dark <dolk_svictor@gmail.com>
2022-07-13 19:40:49 +0000

    second phase from pointers arrays strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,32 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int k = 0, i = 0;
+int k, i = 0, j = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
+while (src[j] != '\0')
+{
+j++;
+}
+if (j < n)
+{
+for (k = 0; k <= j; k++)
+{
+*(dest + k) = *(src + k);
+}
+for (; k < n; k++)
+{
+*(dest + k + 1) = '\0';
+}
+}
+else
+{
 for (k = 0; k < n; k++)
 {
 *(dest + k) = *(src + k);
 }
-for ( ; k < i; k++)
-{
-*(dest + k) = '\0';
 }
 return (dest);
 }
