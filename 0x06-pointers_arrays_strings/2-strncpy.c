7bb7a50024da72930744e067fbbf8877ee216a44
harkinder-dark <dolk_svictor@gmail.com>
2022-07-14 11:09:31 +0000

    second phase from pointers arrays strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-*_strncpy - Write a function that copies a string
+*cpy_sub - Write a function that copies a string
 *@dest: parameter
 *@src: parameter
 *@n: parameter
 *Return: string
 */
-char *_strncpy(char *dest, char *src, int n)
+char *cpy_sub(char *dest, char *src, int n)
 {
 int k, i = 0, j = 0;
 while (dest[i] != '\0')
@@ -18,7 +18,7 @@ while (src[j] != '\0')
 {
 j++;
 }
-if (j < n)
+if (j <= n)
 {
 for (k = 0; k <= j; k++)
 {
@@ -37,4 +37,67 @@ for (k = 0; k < n; k++)
 }
 }
 return (dest);
+}
+
+/**
+*cpy_sup - Write a function that copies a string
+*@dest: parameter
+*@src: parameter
+*@n: parameter
+*Return: string
+*/
+char *cpy_sup(char *dest, char *src, int n)
+{
+int k, i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+if (j <= i)
+{
+for (k = 0; k <= j; k++)
+{
+*(dest + k) = *(src + k);
+}
+for (; k < i; k++)
+{
+*(dest + k + 1) = '\0';
+}
+}
+else
+{
+for (k = 0; k <= i; k++)
+{
+*(dest + k) = *(src + k);
+}
+}
+return (dest);
+}
+
+/**
+*_strncpy - Write a function that copies a string
+*@dest: parameter
+*@src: parameter
+*@n: parameter
+*Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int k, i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+if (i >= n)
+return (cpy_sub(dest, src, n));
+else
+return (cpy_sup(dest, src, n));
 }
