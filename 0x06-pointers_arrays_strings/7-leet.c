432bc337409bd0becb8b5a42c8602b27726a668a
harkinder-dark <dolk_svictor@gmail.com>
2022-07-14 13:37:34 +0000

    second phase from pointers arrays strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,29 +8,19 @@
 char *leet(char *c)
 {
 int i = 0, j = 0;
+char *s = "abcdefghijklmnopqrstuvwxyz";
+char *t = "4bcd3fghijk1mn0pqrs7uvwxyz";
+char *u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 while (c[i] != '\0')
 {
-if (c[i] == 'a' || c[i] == 'A')
+for (j = 0; j < 26; j++)
 {
-*(c + i) = '4';
-}
-if (c[i] == 'e' || c[i] == 'E')
+if (c[i] == s[j] || c[i] == u[j])
 {
-*(c + i) = '3';
-}
-if (c[i] == 'o' || c[i] == 'O')
-{
-*(c + i) = '0';
-}
-if (c[i] == 't' || c[i] == 'T')
-{
-*(c + i) = '7';
-}
-if (c[i] == 'l' || c[i] == 'L')
-{
-*(c + i) = '1';
+*(c + i) = *(t + j);
 }
 i++;
+}
 }
 return (c);
 }
