2440a1df5a2f7a22663d5266c467239d3460f32a
harkinder-dark <dolk_svictor@gmail.com>
2022-07-18 19:11:40 +0000

    firth programming of the pointers arrays strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,18 +3,18 @@
 /**
 *print_diagsums -  sum of the two diagonals of a square
 *matrix of integers.
-@a: parameter
-@size: parameter
+*@a: parameter
+*@size: parameter
 */
 void print_diagsums(int *a, int size)
 {
-int i, j , k, trace = 0, inv = 0;
+int i, j, k, trace = 0, inv = 0;
 int **b = &a;
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-for(j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
-if(i == j)
+if (i == j)
 {
 trace += b[i][j];
 }
