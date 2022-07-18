7c8557845370649e2dc6f398c41e56dc2ea860e4
harkinder-dark <dolk_svictor@gmail.com>
2022-07-18 19:13:53 +0000

    firth programming of the pointers arrays strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,13 @@
 void print_diagsums(int *a, int size)
 {
 int i, j, k, trace = 0, inv = 0;
-int **b = &a;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
 if (i == j)
 {
-trace += b[i][j];
 }
-inv += b[i][(size - 1) - i];
 }
 }
 printf("%d, %d", trace, inv);
