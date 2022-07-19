347a825717eaf961b874f270b09ceaa3f07e6237
harkinder-dark <dolk_svictor@gmail.com>
2022-07-19 19:48:17 +0000

    firth programming of the pointers arrays strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,15 @@
 void print_chessboard(char (*a)[8])
 {
 int i = 0, j = 0, k = 0;
-while (i < 8)
+while (**(a + k) != '\0')
 {
-j = 0;
-while (j < 8)
+k++;
+}
+while (**(a + i) != '\0')
+{
+for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
-j++;
 }
 _putchar(10);
 i++;
