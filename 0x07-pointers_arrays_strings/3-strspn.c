d6186ce7175de0a956bcb7c298e2a1112917fa8e
harkinder-dark <dolk_svictor@gmail.com>
2022-07-19 19:32:23 +0000

    firth programming of the pointers arrays strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,31 +7,25 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i = 0, k = 0, j = 0, p = 0, z = 0;
-int m = _putchar(32);
-while (accept[j] != '\0')
+int i = 0, j = 0, k = 0;
+char *a = s, *b = accept;
+while (a[i] != '\0')
 {
+j = 0;
+while (b[j] != '\0')
+{
+if (b[j] == a[i])
+{
+k++;
+break;
+}
 j++;
 }
-while (s[i] != '\0')
+if (s[i] != accept[j])
 {
-while (p <= j || s[i] == m)
-{
-if (s[i] == accept[j])
-{
-z++;
-}
-p++;
-}
-if (z == (i - k + 1))
-{
-return (z);
-}
-if (s[i] == m)
-{
-k = i + 1;
+break;
 }
 i++;
 }
-return (0);
+return (k);
 }
