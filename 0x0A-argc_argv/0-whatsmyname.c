c14f4a88b8c76710b20304e50db274f840ff7619
harkinder-dark <victorvaddely@gmail.com>
2022-07-22 20:16:13 +0000

    main arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 *@argv: vector argument
 *Return: 0 always
 */
-int main(int argc __attribute__((unused)), int *argv[])
+int main(int argc  __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 return (0);
