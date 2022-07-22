7927a9874970c2ef48f58d4436159e4ea7c17263
harkinder-dark <victorvaddely@gmail.com>
2022-07-22 20:54:39 +0000

    main arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,15 @@
 */
 int main(int argc, char *argv[])
 {
+int i;
 printf("%s", argv[0]);
 if (argc > 1)
 {
-while (*argv != '\0')
+i = 1;
+while (argv[i] != '\0')
 {
-printf("\n%s", *argv);
-argv++;
+printf("\n%s", argv[i]);
+i++;
 }
 }
 return (0);
