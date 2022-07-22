d314b7b0a27ca67f0ca1145534f2aafbbc1a6379
harkinder-dark <victorvaddely@gmail.com>
2022-07-22 21:25:36 +0000

    main arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
 *main - multiplies
 *@argc: parameter
@@ -11,12 +10,12 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
 	{
-		printf("Error");
+		printf("%s\n", "Error");
 		return (1);
 	}
 }
