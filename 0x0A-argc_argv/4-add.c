4fc37857979e46ddf4562776054b221f6150b145
harkinder-dark <victorvaddely@gmail.com>
2022-07-23 10:05:18 +0000

    main arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,28 +9,36 @@
 */
 int main(int argc, char *argv[])
 {
-int i, j, k = 0;
-if (argc < 1)
-{
-printf("%d\n", 0);
-return (-1);
-}
-else
-{
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (sizeof(atoi(&argv[i][j])) != sizeof(int))
-{
-printf("%s\n", "Error");
-return (1);
-}
-k += atoi(argv[i]);
-}
-}
-printf("%d\n", k);
-return (0);
-}
+int i, j, p, k = 0;
+char *c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+		return (-1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				for (p = 0; p < 52; p++)
+				{
+					if (argv[i][j] == c[p])
+					{
+						printf("%s\n", "Error");
+						return (1);
+					}
+					else
+					{
+						continue;
+					}
+				}
+			}
+			k += atoi(argv[i]);
+		}
+		printf("%d\n", k);
+		return (0);
+	}
 }
 
