707dd3730957faf21ebf7fe92dd98b909aa4c5b6
harkinder-dark <victorvaddely@gmail.com>
2022-07-23 09:44:40 +0000

    main arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (sizeof(atoi(argv[i][j])) != sizeof(int))
+if (sizeof(atoi(&argv[i][j])) != sizeof(int))
 {
 printf("%s\n", "Error");
 return (1);
