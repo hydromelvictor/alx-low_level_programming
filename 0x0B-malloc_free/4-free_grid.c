49681753a2eae8058fceec0cb1c55840ec112c6c
harkinder-dark <victorvaddely@gmail.com>
2022-07-25 21:25:40 +0000

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdi.h>
+#include < stdlib.h>
+#include "main.h"
+/**
+*free_grid -  frees a 2 dimensional grid previously created
+*@grid: parameter
+*@height: parameter
+*/
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
