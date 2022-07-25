1d1314c54016ab5c0fa8f99db24dbba37f359da3
harkinder-dark <victorvaddely@gmail.com>
2022-07-25 16:23:36 +0000

    concatenates two strings

--- 0x0B-malloc_free/str_concat.c
+++ 0x0B-malloc_free/str_concat.c
@@ -11,53 +11,11 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0, k;
 	char *str;
-	if (*s1 == NULL && *s2 == NULL)
+	if(*s1 == NULL && *s2 == NULL)
 	{
 		return (NULL);
 	}
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	str = malloc((i + j + 1) * sizeof(char));
-	if (str == NULL) 
-	{
-		return (NULL);
-	}
-	if (i == 0)
-	{
-		for (k = 0; k <= j; k++)
-		{
-			str[k] = s2[k];
-		}
-		str[k + 1] = '\0';
-		return (str);
-	}
-	else if (j == 0)
-	{
-		for (k = 0; k <= i; k++)
-                {
-                        str[k] = s1[k];
-                }
-                str[k + 1] = '\0';
-                return (str);
-	}
-	else
-	{
-		for (k = 0; k <= i; k++)
-                {
-                        str[k] = s1[k];
-                }
-		for (; k <= j; k++)
-                {
-                        str[k] = s2[k];
-                }
-                str[k + 1] = '\0';
-                return (str);
-	}
+	return (1);
+
 }
 
