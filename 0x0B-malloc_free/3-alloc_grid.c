14eb5a05d65d263f75c0218bbd01b045dd51ccf9
harkinder-dark <victorvaddely@gmail.com>
2022-07-25 21:14:22 +0000

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,32 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int **tab, i, j;
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		tab = malloc(height * sizeof(*tab));
-		if (tab == NULL)
-		{
-			return (NULL);
-		}
-		for (i = 0; i < height; i++)
-		{
-			tab[i] = malloc(width * sizeof(*(tab[i])));
-			if (tab[i] == NULL)
-			{
-				return (NULL);
-			}
-			for (j = 0; j <= width; j++)
-                        {
-                                tab[i][j] = 0;
-                        }
-		}
-		return (tab);
-	}
+int **tab, i, j;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+else
+{
+tab = malloc(height * sizeof(*tab));
+if (tab == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+tab[i] = malloc(width * sizeof(*(tab[i])));
+if (tab[i] == NULL)
+{
+return (NULL);
+}
+for (j = 0; j <= width; j++)
+{
+tab[i][j] = 0;
+}
+}
+return (tab);
+}
 }
 
