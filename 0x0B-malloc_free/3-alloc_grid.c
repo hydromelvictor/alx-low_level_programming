1f11d36c11e53f1f66587d938493bec0f6258929
harkinder-dark <victorvaddely@gmail.com>
2022-07-25 21:06:55 +0000

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,6 +33,7 @@ int **alloc_grid(int width, int height)
                         {
                                 tab[i][j] = 0;
                         }
+			free(tab[i]);
 		}
 		return (tab);
 	}
