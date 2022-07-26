ccbd8732f5f984065f05794a0a4a5dcfd33a429e
harkinder-dark <victorvaddely@gmail.com>
2022-07-26 12:03:10 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,24 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0, k = 0, f = 0;
 	char *str;
-	while (s1 != NULL && s1[i] != '\0')
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	while (&& s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2 != NULL && s2[j] != '\0')
+	while (s2[j] != '\0')
 	{
 		j++;
 	}
 	str = malloc((i + j) * sizeof(char) + 1);
-	if ((i == 0 && j == 0) || str == NULL || (s1 == NULL && s2 == NULL))
+	if (str == NULL)
 	{
 		return (NULL);
 	}
