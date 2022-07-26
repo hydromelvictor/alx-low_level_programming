f3ff07536fd7afe253b4570bef356f9712c9bc38
harkinder-dark <victorvaddely@gmail.com>
2022-07-26 11:37:41 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		j++;
 	}
-	str = malloc((i + j + 1) * sizeof(char));
+	str = malloc((i + j) * sizeof(char) + 1);
 	if ((i == 0 && j == 0) || str == NULL || (s1 == NULL && s2 == NULL))
 	{
 		return (NULL);
