ef140a7f557913c7f67faffc18946bda7b415340
harkinder-dark <victorvaddely@gmail.com>
2022-07-28 12:42:22 +0000

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlid.h>
+#include <stdlib.h>
 #include <limits.h>
 #include "main.h"
 /**
@@ -8,8 +8,7 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
-	p = malloc(b);
+	void *p = malloc(b);
 	if (p == NULL)
 	{
 		exit(98);
