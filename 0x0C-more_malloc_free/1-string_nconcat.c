9a548f8deb6bc837e9b374154d53db585ed61bac
harkinder-dark <victorvaddely@gmail.com>
2022-07-28 17:03:22 +0000

    allocates memory using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i = strlen(s1), j = strlen(s2), k = 0;
-unsigned int p = (n >= j) ? i + j : i + n, s;
+unsigned int s;
 char *str = (n >= j) ? malloc((i * j) * sizeof(char) + 1) :
 malloc((i * n) * sizeof(char) + 1);
 if (str == NULL || (s1 == NULL && s2 == NULL))
@@ -28,6 +28,7 @@ str[k] = s1[k];
 }
 if (j != 0)
 {
+j = (n >= j) ? j : n;
 s = 0;
 k = k + 1;
 for (s = 0; s < j; s++)
