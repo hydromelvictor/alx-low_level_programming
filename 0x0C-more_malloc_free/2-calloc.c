07a3fc733b041e9ecd4db79389a85365c1947735
harkinder-dark <victorvaddely@gmail.com>
2022-07-28 17:37:03 +0000

    allocates memory using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 int *tab = malloc(nmemb * size);
 if (tab == NULL)
 {
