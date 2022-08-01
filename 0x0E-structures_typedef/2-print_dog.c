9d5d7510ccf99c1d6ce352904e5c26deede09203
harkinder-dark <victorvaddely@gmail.com>
2022-08-01 15:53:33 +0000

    struct and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -24,6 +24,13 @@ if (d->owner == NULL)
 d->owner = str;
 printf("Owner: %s", "(nil)");
 }
+if(!d->age)
+{
+printf("Name: %s\nAge: %s\nOwner: %s", d->name, str, d->owner);
+}
+else
+{
 printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+}
 }
 }
