7eecadc8be6d9b3a7d21e8c283935af4995ce6e0
harkinder-dark <victorvaddely@gmail.com>
2022-08-01 13:03:32 +0000

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define str     __FILE__
 /**
  * main - print the name of the file
  * Return: 0 always
@@ -7,7 +8,6 @@
 int main(void)
 {
 int i = 0;
-const char *str = __FILE__;
 while (str[i] != '\0')
 {
 putchar(str[i]);
