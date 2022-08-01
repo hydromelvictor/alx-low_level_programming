6dfc58a4279550317ee7c30bff1a6786f4fa3190
harkinder-dark <victorvaddely@gmail.com>
2022-08-01 16:12:08 +0000

    struct and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *wouh = malloc(sizeof(dog_t));
-if(wouh == NULL)
+if (wouh == NULL)
 {
 return (NULL);
 }
-wouh->name = name;
+*(wouh)->name = *name;
 wouh->age = age;
-wouh->owner = owner;
+*(wouh)->owner = *owner;
 return (wouh);
 }
