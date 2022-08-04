846d4dee4bb8bf5b5b65e3576b4cae1c073e86e8
harkinder-dark <victorvaddely@gmail.com>
2022-08-04 21:31:45 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,10 +2,63 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+void entier(va_list list)
+{
+printf("%d", va_arg(list, int));
+}
+void variant(va_list list)
+{
+printf("%lf", va_arg(list, double));
+}
+void word(va_list list)
+{
+printf("%s", va_arg(list, char *));
+}
 /**
  * print_all - print anything
  * 
  * @format: parameter 
  * 
  */
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
+{
+va_list list;
+int i = 0;
+char *str = malloc(sizeof(char));
+if (format == NULL || str == NULL)
+{
+printf("\n");
+return;
+}
+va_start(list, format);
+void (*show[])(va_list) = {entier, variant, word};
+while (format[i] != '\0')
+{
+if (format[i] == 'c' || format[i] == 's')
+{
+(*show[2])(list);
+if (format[i + 1] != '\0')
+{
+printf(", ");
+}
+}
+if (format[i] == 'i')
+{
+(*show[0])(list);
+if (format[i + 1] != '\0')
+{
+printf(", ");
+}
+}
+if (format[i] == 'f')
+{
+(*show[1])(list);
+if (format[i + 1] != '\0')
+{
+printf(", ");
+}
+}
+i++;
+}
+printf("\n");
+}
