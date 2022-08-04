2fad3d675048ed55d0571e97d123969cc3e2bb31
harkinder-dark <victorvaddely@gmail.com>
2022-08-04 07:50:40 +0000

    function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "function_pointers.h"
 /**
  * @brief 
  * 
  */
+int main(int argc, char *argv[])
+{
+    int i, j, k = 0;
+    char str[17] = "0123456789abcdef";
+    for (i = 0; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            printf("Error\n");
+            exit(2);
+        }
+        for (j = 0; j < 17; j++)
+        {
+            if (argv[i][0] != str[j] || argv[i][1] != str[j])
+            {
+                k++;
+            }
+        }
+        if (k >=16)
+        {
+            printf("Error\n");
+            exit(1);
+        }
+        printf("%d", atoi(argv[i]));
+    }
+
+}
