f91182999a1a92662dfb2a36301b78112323f9d6
harkinder-dark <victorvaddely@gmail.com>
2022-08-04 07:53:59 +0000

    function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,33 +3,35 @@
 #include <string.h>
 #include "function_pointers.h"
 /**
- * @brief 
- * 
+ * main - opcodes
+ * @argc: parameter
+ * @argv: parameter
+ * Return: hexadecimal integer
  */
 int main(int argc, char *argv[])
 {
-    int i, j, k = 0;
-    char str[17] = "0123456789abcdef";
-    for (i = 0; i < argc; i++)
-    {
-        if (argv[i][0] == '-')
-        {
-            printf("Error\n");
-            exit(2);
-        }
-        for (j = 0; j < 17; j++)
-        {
-            if (argv[i][0] != str[j] || argv[i][1] != str[j])
-            {
-                k++;
-            }
-        }
-        if (k >=16)
-        {
-            printf("Error\n");
-            exit(1);
-        }
-        printf("%d", atoi(argv[i]));
-    }
-
+int i, j, k = 0;
+char str[17] = "0123456789abcdef";
+for (i = 0; i < argc; i++)
+{
+if (argv[i][0] == '-')
+{
+printf("Error\n");
+exit(2);
+}
+for (j = 0; j < 17; j++)
+{
+if (argv[i][0] != str[j] || argv[i][1] != str[j])
+{
+k++;
+}
+}
+if (k >=16)
+{
+printf("Error\n");
+exit(1);
+}
+printf("%d", atoi(argv[i]));
+}
+return (0);
 }
