e9fa313ce526c0afec371e7f0a5acbf55a998f04
harkinder-dark <victorvaddely@gmail.com>
2022-08-05 11:34:37 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,78 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 /**
- * entier - function
- *
- * @list: parameter
- */
-void entier(va_list list)
-{
-printf("%d", va_arg(list, int));
-}
-/**
- * variant - function
- *
- * @list: parameter
- */
-void variant(va_list list)
-{
-printf("%f", va_arg(list, float));
-}
-/**
- * word - function
- *
- * @list: parameter
- */
-void word(va_list list)
-{
-printf("%s", va_arg(list, char *));
-}
-/**
- * print_all - print anything
- *
- * @format: parameter
+ * print_all - print anything 
  * 
+ * @format: parameter
+ *
  */
 void print_all(const char * const format, ...)
 {
 va_list list;
-int i = 0;
+int i = 0, j = 0;
 if (format == NULL)
 {
 printf("\n");
 return;
 }
 va_start(list, format);
-void (*show[])(va_list) = {entier, variant, word};
-while (format[i] != '\0')
+while (*(format + j) != '\0')
 {
-if (format[i] == 'c' || format[i] == 's')
+if (*format == 'c' || *format == 's')
 {
-(*show[2])(list);
-if (format[i + 1] != '\0')
-{
-printf(", ");
-}
-}
-if (format[i] == 'i')
-{
-(*show[0])(list);
-if (format[i + 1] != '\0')
+printf("%s", va_arg(list, const char *));
+if (*(format + j + 1) != '\0')
 {
 printf(", ");
 }
 }
-if (format[i] == 'f')
+if (*format == 'i')
 {
-(*show[1])(list);
-if (format[i + 1] != '\0')
+printf("%d", va_arg(list, int));
+if (*(format + j + 1) != '\0')
 {
 printf(", ");
 }
 }
-i++;
+if (*format == 'f')
+{
+printf("%f", (float) va_arg(list, double));
+if (*(format + j + 1) != '\0')
+{
+printf(", ");
 }
+}
+j++;
+}
+va_end(list);
 printf("\n");
 }
