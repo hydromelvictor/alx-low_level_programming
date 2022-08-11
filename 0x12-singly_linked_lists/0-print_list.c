6ba9d29ddb740e8c7e939baa34a716527b9e51e8
harkinder-dark <victorvaddely@gmail.com>
2022-08-11 12:25:07 +0000

    linked list programming

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,47 +10,10 @@
  */
 size_t print_list(const list_t *h)
 {
-int i = 0;
 size_t count = 0;
 if (h == NULL)
 {
-return (0);
-}
-while (h != NULL)
-{
-_putchar('[');
-if (h->str == NULL)
-{
-_putchar(0 + '0');
-}
-else
-{
-number(h->len);
-}
-_putchar(']');
-_putchar(32);
-if (h->str == NULL)
-{
-_putchar('(');
-_putchar('n');
-_putchar('i');
-_putchar('l');
-_putchar(')');
-}
-else
-{
-while (h->str[i])
-{
-_putchar(h->str[i]);
-i++;
-}
-}
-if (h != NULL)
-{
-_putchar(10);
-}
-h = h->next;
-count++;
+    return (count);
 }
 return (count);
 }
