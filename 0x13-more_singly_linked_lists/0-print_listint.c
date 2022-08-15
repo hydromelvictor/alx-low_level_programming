145a0a5184cc82d336814af3e057a4fa8d151479
harkinder-dark <victorvaddely@gmail.com>
2022-08-15 15:41:42 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,26 +5,22 @@
  * print_listint - prints all the elements of a listint_t list
  *
  * @h: parameter
- * @size_t: parameter
+ * Return: size_t
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
-    listint_t *nbre = malloc(sizeof(listint_t));
-    {
-        /* code */
-    }
-    
-    if (h == NULL)
-    {
-        return (count);
-    }
-    *nbre = *h;
-    while (nbre)
-    {
-        printf("%d\n", nbre->n);
-        count++;
-        nbre = nbre->next;
-    }
-    return (count);
+size_t count = 0;
+listint_t *nbre = malloc(sizeof(listint_t));
+if (h == NULL)
+{
+return (count);
+}
+*nbre = *h;
+while (nbre)
+{
+printf("%d\n", nbre->n);
+count++;
+nbre = nbre->next;
+}
+return (count);
 }
