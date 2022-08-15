8ea7e052ed77e98c16e40568c843099d6acddc4c
harkinder-dark <victorvaddely@gmail.com>
2022-08-15 16:40:07 +0000

    more singly linked lists programming

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: parameter
+ */
+void free_listint(listint_t *head)
+{
+listint_t *up = malloc(sizeof(listint_t));
+if (up == NULL || head == NULL)
+{
+return;
+}
+up = head;
+head = head->next;
+while (head)
+{
+up->next == NULL;
+free(up);
+head = head->next;
+up = head;
+}
+}
