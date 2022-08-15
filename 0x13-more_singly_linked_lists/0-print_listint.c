dbe173b27f6dcbddd7c5a17b7b774e1f54521ff9
harkinder-dark <victorvaddely@gmail.com>
2022-08-15 15:35:42 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: parameter
+ * @size_t: parameter
+ */
 size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
