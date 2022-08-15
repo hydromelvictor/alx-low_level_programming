cc03327583d25c1fc368edaddbdf53879bf64750
harkinder-dark <victorvaddely@gmail.com>
2022-08-15 15:51:32 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,14 +7,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}listint_t;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 #endif
