79b1c579b351cc39d39c0dfb11bac3ebab3daef6
harkinder-dark <victorvaddely@gmail.com>
2022-08-16 20:44:57 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,15 +2,15 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given
  * position
- * 
+ *
  * @head: parameter
  * @idx: parameter
  * @n: parameter
- * Return: listint_t* 
+ * Return: listint_t*
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0;    
+unsigned int i = 0;
 listint_t *new = malloc(sizeof(listint_t)), *start = malloc(sizeof(listint_t));
 if (new == NULL || start == NULL)
 {
@@ -18,8 +18,15 @@ return (NULL);
 }
 start = *head;
 new->n = n;
-while(start != NULL)
+if (idx == 0)
 {
+new->next == start;
+*head = new;
+return (new);
+}
+while (start != NULL)
+{
+
 i++;
 if (i == idx)
 {
