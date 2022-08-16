a063bde38f394b88d921b36ca2c265b372119d88
harkinder-dark <victorvaddely@gmail.com>
2022-08-16 08:04:21 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,17 @@
  */
 void free_listint(listint_t *head)
 {
+if (head == NULL)
+{
+return;
+}
 while (head->next != NULL)
 {
+free(&head->n);
 free(head);
 head = head->next;
 }
+free(&head->n);
 free(head);
 }
 
