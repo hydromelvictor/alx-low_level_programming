a81f32cf335a108d576f9023821f8fc2b6cec741
harkinder-dark <victorvaddely@gmail.com>
2022-08-16 19:26:38 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stddef.h>
 /**
  * get_nodeint_at_index - Get the nodeint at index object
  *
@@ -9,9 +10,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {;
 unsigned int i = 0;
-if (head == NULL)
+while (head != NULL)
 {
-return (NULL);
+i++;
+if (i == index + 1)
+{
+return (head);
 }
-
+}
+return (NULL);
 }
