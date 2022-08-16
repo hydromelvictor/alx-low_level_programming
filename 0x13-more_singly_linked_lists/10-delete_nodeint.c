4b6882ea18073682180401dd945ae35a44581a62
harkinder-dark <victorvaddely@gmail.com>
2022-08-16 22:06:22 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,21 +1,32 @@
 #include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node at index index
+ * of a listint_t linked list
+ *
+ * @head: parameter
+ * @index: parameter
+ * Return: int
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *del = malloc(sizeof(listint_t));
 listint_t *start = malloc(sizeof(listint_t));
-if (del == NULL || head == NULL || start == NULL)
+if (del == NULL || start == NULL)
 {
 return (-1);
 }
-*start = **head;
-while (i < index)
+start = *head;
+while (start != NULL)
 {
-start = start->next;
-}
+i++;
+if (i == index - 1)
+{
 del = start->next;
-start->next = start->next->next;
-free(&del->n);
+start->next = del->next;
 free(del);
-retrun (1);
+return (1);
+}
+}
+return (-1);
 }
