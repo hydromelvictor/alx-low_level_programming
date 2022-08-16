827c61ace31b43c307ba707a108884f76f7753e0
harkinder-dark <victorvaddely@gmail.com>
2022-08-16 19:00:21 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,19 +1,23 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index - Get the nodeint at index object
+ *
+ * @head: parameter
+ * @index: parameter
+ * Return: listint_t*
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 size_t count = listint_len(head);
 unsigned int i = 0;
-if (index > count - 1 || head == NULL)
+if (index > count || head == NULL)
 {
 return (NULL);
 }
-else
-{
-while (i < index)
+while (i <= index)
 {
 head = head->next;
 i++;
 }
 return (head);
-}
 }
