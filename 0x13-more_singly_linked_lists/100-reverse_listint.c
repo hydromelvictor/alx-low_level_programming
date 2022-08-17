4b4f3195dc6325b536b6af56014d9a1fa1d34351
harkinder-dark <victorvaddely@gmail.com>
2022-08-17 13:07:03 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,17 +5,11 @@
 /**
  * reverse_listint - funnctions reverse
  *
- * head: parameter
+ * @head: parameter
  * Return: listint_t*
  */
 listint_t *reverse_listint(listint_t **head)
 {
-if (*head != NULL)
-{
-if ((**head).next == NULL)
-{
-return (*head)
-}
-}
+
 return (*head);
 }
