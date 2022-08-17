a90514f71682c2b8dfbe7e1e55c07d9ec54eff24
harkinder-dark <victorvaddely@gmail.com>
2022-08-17 12:51:45 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,17 +4,7 @@
 #include "lists.h"
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *current = *head, *prev = current->next;
-if (head == NULL)
-{
-return (NULL);
-}
-if (prev == NULL)
-{
-return (head);
-}
-prev->next = reverse_listint(prev);
-current->next = NULL;
-return (head);
+
+return (*head);
 }
 
