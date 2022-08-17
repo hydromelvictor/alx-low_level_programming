ebab0564cca848c53586a20aaa20fb56a72794c1
harkinder-dark <victorvaddely@gmail.com>
2022-08-17 12:54:47 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,9 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * reverse_listint - funnctions reverse
+ *
+ * head: parameter
+ * Return: listint_t*
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 
 return (*head);
 }
-
