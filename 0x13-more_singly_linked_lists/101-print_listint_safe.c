4e16b9f490762f59db31394a7bb0fcf78d3646c9
harkinder-dark <victorvaddely@gmail.com>
2022-08-17 13:38:17 +0000

    more singly linked lists programming

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,18 +1,17 @@
 #include "lists.h"
+/**
+ * print_istint_safe - print lists
+ * 
+ * @head: parameter
+ * Return: size_t
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
 listint_t *nbre = malloc(sizeof(listint_t));
-if (head == NULL)
+if (head == NULL || nbre == NULL)
 {
 exit(98);
 }
-*nbre = *head;
-while (nbre)
-{
-printf("%d\n", nbre->n);
-count++;
-nbre = nbre->next;
-}
+
 return (count);
 }
