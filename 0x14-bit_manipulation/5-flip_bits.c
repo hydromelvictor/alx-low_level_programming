7d42aab5691174463abd337f80ed7da8f3cc3bf2
harkinder-dark <victorvaddely@gmail.com>
2022-08-18 16:35:09 +0000

    ALX bit manipulation tasks0

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-unsigned int *binary(unsigned long int n)
+unsigned char *binary(unsigned long int n)
 {
 unsigned long int mask, i, z = n, x = 0;
-unsigned int *s = malloc(sizeof(unsigned int));
+unsigned char *s = malloc(sizeof(unsigned char));
 if (s == NULL)
 {
 return (NULL);
@@ -17,7 +17,7 @@ x++;
 mask = 1 << x;
 for (i = 1; i <= x + 1; ++i)
 {
-s[i] = (n & mask ? 1 : 0);
+s[i] = (n & mask ? '1' : '0');
 n <<= 1;
 }
 return (s);
@@ -32,10 +32,11 @@ return (s);
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int s = n ^ m;
-unsigned int *d = binary(s), i = 0, j = 0;
+unsigned int i = 0, j = 0;
+unsigned char *d = binary(s);
 while (d[i] != '\0')
 {
-if (d[i] == 1)
+if (d[i] == '1')
 {
 j++;
 }
