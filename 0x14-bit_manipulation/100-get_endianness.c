110dd21ec41006efdb1a45231c6aa34957ed2abe
harkinder-dark <victorvaddely@gmail.com>
2022-08-18 17:54:22 +0000

    ALX bit manipulation tasks0

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,16 +2,19 @@
 #include "main.h"
 /**
  * get_endianness - Get the endianness object
- * 
+ *
  * Return: int
  */
 int get_endianness(void)
 {
-    unsigned int value = 0x1;
-    char *r = (char *) &value;
- 
-    if (*r == 1) 
-        return 1;
-    else
-        return 0; 
+unsigned int value = 0x1;
+char *r = (char *) &value;
+if (*r == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
