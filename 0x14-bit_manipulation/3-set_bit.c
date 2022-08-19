1a7d03fc1c08d82a2d16c2a0a22dc9ca3d1e8e3c
harkinder-dark <victorvaddely@gmail.com>
2022-08-19 05:43:17 +0000

    ALX bit manipulation tasks0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,19 +10,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int z = n, x = 0;
 if (n != NULL)
-{
-while (z > 1)
-{
-z >>= 1;
-x++;
-}
-if (index <= x)
 {
 *n |= (1 << index);
 return (1);
-}
 }
 return (-1);
 }
