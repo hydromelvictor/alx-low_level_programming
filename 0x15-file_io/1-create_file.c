46e5475f12556bbd6c1459368239867c38e40c45
harkinder-dark <victorvaddely@gmail.com>
2022-08-22 22:23:32 +0000

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * create_file - Create a file object
+ * 
+ * @filename: parameter
+ * @text_content: parameter
+ * Return int
+ */
 int create_file(const char *filename, char *text_content)
 {
 int fd, err, count = 0;
@@ -13,7 +19,7 @@ if (filename == NULL)
 return (-1);
 }
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (fd == -1 || filename == NULL)
+if (fd == -1)
 {
 close(fd);
 return (-1);
