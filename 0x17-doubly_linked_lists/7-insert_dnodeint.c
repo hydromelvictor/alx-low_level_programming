c09e7b6d2c4a1c58fe629484bafa17702ac012fc
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 14:45:28 +0000

    17th day C learning

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -52,7 +52,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
                 }
                 current = current->next;
             }
-            if (s == idx)
+            if (s + 1 == idx)
             {
                 new->next = NULL;
                 new->prev = current;
