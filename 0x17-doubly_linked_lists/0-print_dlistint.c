3f9705e7032fdcba0965828c9ed47a6d3984f2f9
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 09:49:37 +0000

    17th day C learning

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,26 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_dlinstint - print of the list
+ * print_dlistint - print of the list
  *
  * @h: constant header the double list
  * Return: size_t
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    dlistint_t *current = malloc(sizeof(dlistint_t));
-    size_t n;
-    if (current == NULL || h == NULL)
-    {
-        free(current);
-        return (0);
-    }
-    *current = *h;
-    for (n = 0; current != NULL; n++)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-    free(current);
-    return (n);
+dlistint_t *current = malloc(sizeof(dlistint_t));
+size_t n;
+if (current == NULL || h == NULL)
+{
+free(current);
+return (0);
+}
+*current = *h;
+for (n = 0; current != NULL; n++)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+free(current);
+return (n);
 }
