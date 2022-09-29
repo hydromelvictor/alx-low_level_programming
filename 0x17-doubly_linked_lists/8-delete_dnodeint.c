7579e56620c3ff60d209fe0fd14f40eca59573ba
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 15:36:58 +0000

    17th day C learning

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete in index
+ *
+ * @head: parameter
+ * @index: parameter
+ * Return: int
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current = *head, *start;
+    unsigned int i;
+    if (*head == NULL)
+    {
+        return (-1);
+    }
+    if (index == 0)
+    {
+        *head = current->next;
+        free(current);
+        return (1);
+    }
+    return (-1);
+}
