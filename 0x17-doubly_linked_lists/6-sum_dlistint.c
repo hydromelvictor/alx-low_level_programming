b0bada411e0a305fd05c15c0cc9d279647b44384
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 13:33:00 +0000

    17th day C learning

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum the lists data
+ *
+ * head: parameter
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int m = 0, n;
+if (head != NULL)
+{
+for (n = 0; head != NULL; n++)
+{
+m += head->n;
+head = head->next;
+}
+}
+return (m);
+}
