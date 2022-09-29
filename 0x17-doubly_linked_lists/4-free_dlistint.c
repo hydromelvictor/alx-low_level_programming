682863d4fdde09642f1e08d34fda79b636eb7564
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 12:25:25 +0000

    17th day C learning

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,11 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current;
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+dlistint_t *current;
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
 }
