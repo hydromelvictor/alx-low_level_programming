e858eec5089a8cbff46c0083f8c2d5653d6fa2da
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 10:02:02 +0000

    17th day C learning

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,18 +10,18 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    dlistint_t *current = malloc(sizeof(dlistint_t));
-    size_t n;
-    if (current == NULL || h == NULL)
-    {
-        free(current);
-        return (0);
-    }
-    *current = *h;
-    for (n = 0; current != NULL; n++)
-    {
-        current = current->next;
-    }
-    free(current);
-    return (n);
+dlistint_t *current = malloc(sizeof(dlistint_t));
+size_t n;
+if (current == NULL || h == NULL)
+{
+free(current);
+return (0);
+}
+*current = *h;
+for (n = 0; current != NULL; n++)
+{
+current = current->next;
+}
+free(current);
+return (n);
 }
