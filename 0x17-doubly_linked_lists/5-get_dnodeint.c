4f3c241322b96216ab8c125e11294f27045abf2e
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 13:21:36 +0000

    17th day C learning

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ *
+ * @head: header 
+ * @index: index
+ * Return: dlistint_t*
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int n;
+if (head != NULL)
+{
+for (n = 0; head != NULL; n++)
+{
+if (n == index)
+{
+return (head);
+}
+head = head->next;
+}
+}
+return (NULL);
+}
