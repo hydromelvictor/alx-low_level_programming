d281baf2ac627616965dcb29daf6146a830adc55
harkinder-dark <victorvaddely@gmal.com>
2022-09-29 15:39:07 +0000

    17th day C learning

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head, *start;
+    dlistint_t *current = *head;
     unsigned int i;
     if (*head == NULL)
     {
