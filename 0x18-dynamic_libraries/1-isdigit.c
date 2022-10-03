2314977da4ed8cdfdd359a72d56e2209b643f7f7
harkinder-dark <victorvaddely@gmal.com>
2022-10-03 09:40:01 +0000

    18th day C learning

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -19,4 +19,3 @@ return (1);
 }
 return (0);
 }
-
--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -9,7 +9,7 @@
  */
 int add(int a, int b)
 {
-    return (a + b);
+return (a + b);
 }
 
 /**
@@ -21,7 +21,7 @@ int add(int a, int b)
  */
 int sub(int a, int b)
 {
-    return (a - b);
+return (a - b);
 }
 
 /**
@@ -33,7 +33,7 @@ int sub(int a, int b)
  */
 int mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
 
 /**
@@ -45,7 +45,7 @@ int mul(int a, int b)
  */
 int div(int a, int b)
 {
-    return (a / b);
+return (a / b);
 }
 
 /**
@@ -57,5 +57,5 @@ int div(int a, int b)
  */
 int mod (int a, int b)
 {
-    return  (a % b);
+return  (a % b);
 }
