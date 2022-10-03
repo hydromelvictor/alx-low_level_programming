32cc08626188f7d832fcdb221e59d3aa1d43e5f6
harkinder-dark <victorvaddely@gmal.com>
2022-10-03 09:13:14 +0000

    18th day C learning

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - addition
+ *
+ * @a : parameter
+ * @b : parameter
+ * Return: int
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - substraction
+ *
+ * @a : parameter
+ * @b : parameter
+ * Return: int
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - multiplication
+ *
+ * @a : parameter
+ * @b : parameter
+ * Return: int
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - division
+ *
+ * @a : parameter
+ * @b : parameter
+ * Return: int
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - modulo
+ *
+ * @a : parameter
+ * @b : parameter
+ * Return: int
+ */
+int mod (int a, int b)
+{
+    return  (a % b);
+}
---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm 0x18.c/101-md5_gm 0x18.c/READMD.md5_gm
