955c7525584b7028be395a736140738c4bfaffee
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-20 16:37:05 +0000

    nth day in C programming

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - key for hash function
+ * @key: key
+ * @size: size
+ * Return: unsigned long int
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+return (hash_djb2(key) % size);
 }
