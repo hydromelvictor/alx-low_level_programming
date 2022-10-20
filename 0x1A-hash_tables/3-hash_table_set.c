18d88e811845f72ca7a32ee52c3aa370fddd6763
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-20 17:45:43 +0000

    nth day in C programming

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,24 +1,57 @@
 #include "hash_tables.h"
 
+/**
+ * add_node - add new node in hash tables
+ * @key: key
+ * @value: value
+ * Return: hash_node_t
+*/
+hash_node_t *add_node(char *key, char *value)
+{
+    hash_node_t *new = malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    new->key = strdup(key);
+    new->value = strdup(value);
+    new->next = NULL;
+    return new;
+}
+
+
+/**
+ * hash_table_set - set hashage
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: int
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int  i;
-
-    if (key == NULL)
+    unsigned long int cle;
+    hash_node_t *node, *new;
+    if (key == NULL || strlen(key) > 1 || value == NULL ||
+    strlen(value) == NULL || ht->array == NULL || ht->size == 0)
     {
         return (0);
     }
+    cle = hash_djb2(key) % ht->size;
+    node = ht->array[cle];
 
-    i = hash_djb2(key);
-    if (ht->array[i] == NULL)
+    while (node != NULL)
     {
-        ht->array[i]->key = key;
-        ht->array[i]->value = strdup(value);
+        if (strcmp(node->key, key) == 0)
+        {
+            node->value = strdup(value);
+            return (1);
+        }
+        node = node->next;
     }
-    else
-    {
-        ht->array[0]->key = key;
-        ht->array[0]->value = strdup(value);
-    }
+
+    new = add_node(key, value);
+    new->next = ht->array[cle];
+    ht->array[cle] = new;
     return (1);
 }
