fdbae2bf2a912cb9495cee12a2bfc7c3d63aab92
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-21 09:58:13 +0000

    nth day in C programming

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -23,9 +23,6 @@ void hash_table_delete(hash_table_t *ht)
             free(del->value);
             free(del);
         }
-        free(ht->array[i]->key);
-        free(ht->array[i]->value);
-        free(ht->array[i]);
     }
     free(ht->array);
     ht->array = NULL;
