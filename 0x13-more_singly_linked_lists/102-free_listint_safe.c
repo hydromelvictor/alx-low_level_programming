9a276b8472cc06987abbd5c8ae9b03b7e1b68db5
harkinder-dark <victorvaddely@gmail.com>
2023-03-10 11:06:39 +0000

    after

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,13 +2,16 @@
 size_t free_listint_safe(listint_t **h)
 {
 listint_t *del;
+size_t n = 0;
 if (h == NULL || *h == NULL)
-return;
+return (0);
 del = *h;
 while (*h != NULL)
 {
+    n++;
     *h = (*h)->next;
     free(del);
     del = *h;
 }
+return (n);
 }
